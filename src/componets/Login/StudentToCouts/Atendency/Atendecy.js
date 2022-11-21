8df12f276da8599b93f503a5397a6eca40ab7ba1
ortaldiamant <109242596+ortaldiamant@users.noreply.github.com>
2022-11-22 01:18:04 +0200

    Update Atendecy.js

--- src/componets/Login/StudentToCouts/Atendency/Atendecy.js
+++ src/componets/Login/StudentToCouts/Atendency/Atendecy.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { GetStudent } from "../../store/student";
 import AddStudent from "../Addstudent/Addstudant";
+
 
 const Atendecy = () => {
 
