c8134e863f5ee7a2cb53eebf41b3223aef436319
ortaldiamant <109242596+ortaldiamant@users.noreply.github.com>
2022-12-12 00:09:24 +0200

    Create Homepage.js

---
+++ src/componets/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import'./Homepage.css';
+import {NavLink} from "react react-router-dom";
+import {useNavigate} from "react-router";
+export default function Homepage(){
+    const nav= useNavigate();
+    return
+     
+        
+
+
+
+    
+}
