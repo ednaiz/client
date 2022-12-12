68125077f0995231fd0f3b4210ca40d1a0e05e7c
ortaldiamant <109242596+ortaldiamant@users.noreply.github.com>
2022-12-13 01:04:46 +0200

    Update UserNavbar.js

--- src/componets/Register/UserNavbar/UserNavbar.js
+++ src/componets/Register/UserNavbar/UserNavbar.js
@@ -5,7 +5,11 @@ import './NavBar.css';
 const UserNavBar = (props) => {
     return ( <nav className="nav">
         <Link to="coursesList" className="link"> רשימת הקורסים </Link>
-        <Link to="LecturersList" className="link">רשימת המרצים </Link>
+        <Link to="Lecturers" className="link">רשימת המרצים </Link>
+        <Link to="myprofil" className="link">פרופיל הסטודנט ם </Link>
+        <Link to="Atendency" className="link">נוכחות ם </Link>
+        <Link to="About" className="link"> דף אודות ם </Link>
+        
         <Link to="exit" className="link" onClick={() => {props.saveLogin(null)}}> יציאה </Link>
         <Link to="" className="link">ניות </Link>
     </nav>
