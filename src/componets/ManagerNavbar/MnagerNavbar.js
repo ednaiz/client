95f55ba4ad57e9622121c84345f78796cade79cf
ortaldiamant <109242596+ortaldiamant@users.noreply.github.com>
2022-12-13 01:04:43 +0200

    Update MnagerNavbar.js

--- src/componets/ManagerNavbar/MnagerNavbar.js
+++ src/componets/ManagerNavbar/MnagerNavbar.js
@@ -7,6 +7,19 @@ const ManagerNavBar = (props) => {
     return ( <nav className="nav">
         <Link to="StudentList" className="link"> רשימת תלמידים </Link>
         <Link to="addStudent" className="link">הוספת תלמיד ר </Link>
+        <Link to="Atendecy" className="link">הוספת תלמיד ר </Link>
+        <Link to="curseslist" className="link">הוספת תלמיד ר </Link>
+        <Link to="studenttocors" className="link">הוספת תלמיד ר </Link>
+        <Link to="studentdetails" className="link">הוספת תלמיד ר </Link>
+        <Link to="lecturer" className="link">הוספת תלמיד ר </Link>
+        <Link to="exit" className="link">הוספת תלמיד ר </Link>
+        
+        
+
+        
+
+        
+        
         <Link to="exit"  className="link" onClick={() => {props.saveLogin(null)}}> יציאה </Link>
     </nav>
     )
