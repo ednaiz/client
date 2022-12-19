d354ce7fbbb2054fa864c29b298f648b4e6e00e8
ortaldiamant <109242596+ortaldiamant@users.noreply.github.com>
2022-12-19 22:11:23 +0200

    Update NavBar.js

--- src/componets/NavBar/NavBar.js
+++ src/componets/NavBar/NavBar.js
@@ -1,35 +1,29 @@
 import React from "react";
-import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import Home from "../pages/Home";
-import Contact from "../pages/Contact";
-import StudentRoom from "../pages/StudentRoom";
-import Pupil from "../pages/Pupil";
+import { Link } from "react-router-dom";
+// import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
+// import Home from "../pages/Home";
+// import Contact from "../pages/Contact";
+// import StudentRoom from "../pages/StudentRoom";
+// import Pupil from "../pages/Pupil";
 
 function Navbar() {
 
     return (
         <div>
-            <Router>
-                <Link to="/">
-                    Home
-                </Link>
+            <Link to="register" > הרשמה</Link>
+            <Link to="/">
+                Home
+            </Link>
+            <Link to="studentsList">
+                Students
+            </Link>
+            {/* <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/contact" component={Contact} />
 
-                <Link to="/contact">
-                    Contact
-                </Link>
-
-                <Link to="/students">
-                    Students
-                </Link>
-
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/contact" component={Contact} />
-
-                    <Route exact path="/students" component={StudentRoom} />
-                    <Route path="/pupil" component={Pupil} />
-                </Switch>
-            </Router>
+                <Route exact path="/students" component={StudentRoom} />
+                <Route path="/pupil" component={Pupil} />
+            </Switch> */}
         </div>
     );
 };
