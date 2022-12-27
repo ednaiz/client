a306ea7b731e58b0455ce9c71b638a5f82f165fb
ortaldiamant <109242596+ortaldiamant@users.noreply.github.com>
2022-12-27 19:00:24 +0200

    Update StudentList.js

--- src/componets/StudentList/StudentList.js
+++ src/componets/StudentList/StudentList.js
@@ -10,16 +10,17 @@ const StudentList = () => {
     const [showAdd, setShowAdd] = useState(false)
 
     useEffect(() => {
-        GetStudent()
-            .then(x => setStudent(x.data))
-            .catch(err => setStudent(
-                [
-                    { FName: "טל", Email: "fHFHFFG", Phone: "055666666777", id: 0, LName: "holon", adress: "hadar", year: 2, nameuser: "tal", pass: "1238889843", status: 0 },
-                    { FName: "עינבר", Email: "dGFGGF", Phone: "056667899996", id: 1, LName: "hederan", adress: "hnamaar", year: 2, nameuser: "telkl", pass: "123488883", status: 0 },
-                    { FName: "הילה", Email: "bFGFGFG", Phone: "05256668777", id: 2, LName: "heifa", adress: "habarr", year: 2, nameuser: "tgghjl", pass: "1777773", status: 0 },
-                ]
-            )
-            )
+        // GetStudent()
+        //     .then(x => setStudent(x.data))
+        //     .catch(err => setStudent(
+        //         [
+        //             { FName: "טל", Email: "fHFHFFG", Phone: "055666666777", id: 0, LName: "holon", adress: "hadar", year: 2, nameuser: "tal", pass: "1238889843", status: 0 },
+        //             { FName: "עינבר", Email: "dGFGGF", Phone: "056667899996", id: 1, LName: "hederan", adress: "hnamaar", year: 2, nameuser: "telkl", pass: "123488883", status: 0 },
+        //             { FName: "הילה", Email: "bFGFGFG", Phone: "05256668777", id: 2, LName: "heifa", adress: "habarr", year: 2, nameuser: "tgghjl", pass: "1777773", status: 0 },
+        //         ]
+        //     )
+        //     )
+    GetStudent();
     }, [])
     const [student, setStudent] = useState([]);
 
