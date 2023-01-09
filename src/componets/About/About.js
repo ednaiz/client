7e4bb16027c692f6d6da870f3eb3ad4e23349872
ednaiz <ednaizmaylov129@gmail.com>
2023-01-09 19:19:43 +0200

    Update About.js

--- src/componets/About/About.js
+++ src/componets/About/About.js
@@ -1,6 +1,9 @@
 const About = () => {
     return (
-        <h1>דף אודות </h1>
+        <h1>דף אודות </h1>,
+        <h2> אתר OnSpot </h2>,
+        <p>אתר למעקב אחר נוכחות של סטודנטים.<b/>קו</p>
+        
     )
 }
 export default About;
